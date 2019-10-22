4c628f97dfe509037e55c209e45f6ec720748cbc
Wackery <32279125+webmsgr@users.noreply.github.com>
2019-10-22 12:27:30 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 from distutils.core import setup, Extension
 import sys
-from Cython.Build import cythonize
+
+def dfunc(arg):
+      return arg
+
+
+try:
+    from Cython.Build import cythonize
+    ext = ".pyx"
+    dfunc = lambda x: cythonize(x)
+except:
+    ext = ".c"
 
 
 
-exts = cythonize([Extension('pyOHOL.OHOL', ['src/OHOL/OHOL.pyx'])])+[Extension('pyOHOL',['src/__main__.py'])]
+exts = dfunc([Extension('pyOHOL.OHOL', ['src/OHOL/OHOL'+ext])])+[Extension('pyOHOL',['src/__main__.py'])]
 
 setup(name='pyOHOL',
       version='1.0',
       ext_modules=exts,
+      requires=["numpy","pygame"]
       )
