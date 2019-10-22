af9e4c438b4c77bba58359dd595ae17c92a8d4a1
Wackery <32279125+webmsgr@users.noreply.github.com>
2019-10-22 11:54:35 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup, Extension
+import sys
+from Cython.Build import cythonize
+
+
+
+exts = [cythonize(Extension('PyOHOL.OHOL', ['src/OHOL.pyx']),Extension('PyOHOL',['src/__main__.py'])]
+setup(name='PyOHOL',
+      version='1.0',
+      ext_modules=exts,
+      )
