fa20735aa0b8944d90719d674bbd648d7f0e4d5e
Wackery <32279125+webmsgr@users.noreply.github.com>
2019-10-23 08:23:02 -0700

    Create mapchunkparser.pyx

---
+++ src/mapchunkparser.pyx
@@ -0,0 +1,7 @@
+import zlib
+
+def parse_chunk(header,compressed):
+    header = header.split()
+    _, width, height, x, y, size, buffersize = header
+    mapdata = zlib.decompress(compressed,bufsize=int(buffersize))
+    
