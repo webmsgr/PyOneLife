9e415d5a2a0b40bdd46a89cfe7dba9c142a2fbd1
Wackery <32279125+webmsgr@users.noreply.github.com>
2019-10-23 10:32:14 -0700

    Update client.pyx

--- src/client.pyx
+++ src/client.pyx
@@ -1,5 +1,9 @@
 import pygame
 import multiprocessing as mp
+import requests
+import numpy
+cimport numpy
+from console_progressbar import ProgressBar
 def read_config(folder):
     pass
 def load(folder):
