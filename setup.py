5aa665f25bb4996daa82506c342fe1016e4e8e45
Restyled.io <commits@restyled.io>
2019-10-23 16:41:23 +0000

    Restyled by yapf

--- setup.py
+++ setup.py
@@ -9,8 +9,12 @@ def dfunc(arg):
     return arg
 
 
-exts = cythonize(
-    [Extension("pyOHOL", ["src/client.pyx", "src/mapchunkparser.pyx", "src/miniz.pyx"])]
-)
+exts = cythonize([
+    Extension("pyOHOL",
+              ["src/client.pyx", "src/mapchunkparser.pyx", "src/miniz.pyx"])
+])
 
-setup(name="pyOHOL", version="1.0", ext_modules=exts, requires=["numpy", "pygame"])
+setup(name="pyOHOL",
+      version="1.0",
+      ext_modules=exts,
+      requires=["numpy", "pygame"])
