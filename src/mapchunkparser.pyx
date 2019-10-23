72d29ff3bc17bc8c953959afdd2750bab543dba9
Wackery <32279125+webmsgr@users.noreply.github.com>
2019-10-23 13:12:33 -0700

    Update mapchunkparser.pyx

--- src/mapchunkparser.pyx
+++ src/mapchunkparser.pyx
@@ -7,6 +7,7 @@ cpdef parse_chunk(bytes header,bytes compressed):
     cdef unsigned long *csize 
     cdef unsigned long cbsize
     cdef unsigned long before
+    cdef bytes *out
     cdef char beforetwo = compressed
     cdef char *step2 = &(beforetwo)
     cdef unsigned char *pSource = <unsigned char *>step2
@@ -16,4 +17,5 @@ cpdef parse_chunk(bytes header,bytes compressed):
     before = <unsigned long>int(size)
     csize = &(before)
     mz_uncompress(mpdata,csize,pSource,cbsize)
+    out = <bytes *>mpdata
     
