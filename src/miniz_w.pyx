36ad1ed61dd8b26934cfc6ab3b8f32edd8705362
Wackery <32279125+webmsgr@users.noreply.github.com>
2019-10-23 11:48:57 -0700

    People who know c are very angry at my code

--- src/miniz_w.pyx
+++ src/miniz_w.pyx
@@ -5,9 +5,9 @@ cdef mz_uncompress(unsigned char *pDest, unsigned long *pDest_len, const unsigne
 
 cpdef py_uncompress(int outlen,bytes source,int sourcelen):
     cdef unsigned char *out
-    cdef unsigned long *destlen = &(<unsigned long>outlen)
+    cdef unsigned long *destlen = <unsigned long *>&(outlen)
     cdef unsigned long slen = <unsigned long>sourcelen
-    cdef unsigned char *inp = &(<unsigned char>source)
+    cdef unsigned char *inp = <unsigned char *>&(source)
     mz_uncompress(out,destlen,inp,slen)
     pyout = <bytes>out
     return pyout
