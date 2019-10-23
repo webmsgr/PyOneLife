d9c78d8c566972bebebc568ce76101fbc9adf013
Wackery <32279125+webmsgr@users.noreply.github.com>
2019-10-23 12:57:48 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ exts = cythonize(
     [
         Extension(
             "pyOHOL.client",
-            ["src/client.pyx", "src/mapchunkparser.pyx", "src/miniz_w.pyx"],
+            ["src/client.pyx", "src/mapchunkparser.pyx"],
         )
     ],
     compiler_directives={"language_level": "3"},
