d0a580e0ba72011137dbcb3e1eb53364ce272166
Wackery <32279125+webmsgr@users.noreply.github.com>
2019-10-23 09:04:34 -0700

    Update mapchunkparser.pyx

--- src/mapchunkparser.pyx
+++ src/mapchunkparser.pyx
@@ -1,7 +1,15 @@
-import zlib
+cimport miniz.miniz as miniz
 
 cpdef parse_chunk(header,compressed):
+    cdef unsigned char *mapdata
+    cdef bytes mpdata
+    cdef miniz.mz_ulong csize 
+    cdef miniz.mz_ulong cbsize
     header = header.split()
-    _, width, height, x, y, size, buffersize = header
-    mapdata = zlib.decompress(compressed,bufsize=int(buffersize))
+    _, width, height, x, y, size, cbuffersize = header
+    cbsize = cbuffersize
+    csize = size
+    miniz.mz_decompress(mapdata,csize,compressed,cbsize)
+    mpdata = mapdata
+    
     
