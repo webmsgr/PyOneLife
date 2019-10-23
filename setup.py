c2720849d506d7fc7a3cd06138f2f86d059c0094
Restyled.io <commits@restyled.io>
2019-10-23 16:40:21 +0000

    Restyled by autopep8

--- setup.py
+++ setup.py
@@ -2,15 +2,16 @@ from distutils.core import setup, Extension
 import os
 from Cython.Build import cythonize
 
+
 def dfunc(arg):
     return arg
 
 
-
-exts = cythonize([Extension('pyOHOL', ['src/client.pyx','src/mapchunkparser.pyx','src/miniz.pyx'])])
+exts = cythonize([Extension('pyOHOL', ['src/client.pyx',
+                                       'src/mapchunkparser.pyx', 'src/miniz.pyx'])])
 
 setup(name='pyOHOL',
       version='1.0',
       ext_modules=exts,
-      requires=["numpy","pygame"]
+      requires=["numpy", "pygame"]
       )
