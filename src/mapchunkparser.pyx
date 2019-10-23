65e697ca09b5fc3ac0ba81ec4ed7beab6f68547e
Wackery <32279125+webmsgr@users.noreply.github.com>
2019-10-23 13:11:38 -0700

    Update mapchunkparser.pyx

--- src/mapchunkparser.pyx
+++ src/mapchunkparser.pyx
@@ -2,15 +2,18 @@ cdef extern from "miniz.h":
     int mz_uncompress(unsigned char *pDest, unsigned long *pDest_len, const unsigned char *pSource, unsigned long source_len);
     
 
-cpdef parse_chunk(header,compressed):
-    cdef bytes mpdata
+cpdef parse_chunk(bytes header,bytes compressed):
+    cdef unsigned char *mpdata
     cdef unsigned long *csize 
     cdef unsigned long cbsize
     cdef unsigned long before
+    cdef char beforetwo = compressed
+    cdef char *step2 = &(beforetwo)
+    cdef unsigned char *pSource = <unsigned char *>step2
     header = header.split()
     _, width, height, x, y, size, cbuffersize = header
-    cbsize = <int>cbuffersize
+    cbsize = <unsigned long>int(cbuffersize)
     before = <unsigned long>int(size)
     csize = &(before)
-    
+    mz_uncompress(mpdata,csize,pSource,cbsize)
     
