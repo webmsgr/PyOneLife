07167f8ec1ce3ff4097821b73b5ac83fa57229b8
Wackery <32279125+webmsgr@users.noreply.github.com>
2019-10-23 10:56:12 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 import os
+import sys
 from distutils.core import Extension
 from distutils.core import setup
 import numpy as np
 from Cython.Build import cythonize
 
+if sys.version[0] == 2:
+    sys.stdout.write("No python 2 support\n")
+    sys.exit(1)
 
 def dfunc(arg):
     return arg
@@ -23,4 +27,4 @@ setup(name="pyOHOL",
       version="1.0",
       ext_modules=exts,
       include_dirs = [np.get_include()],
-      requires=["numpy", "pygame","console_progressbar","requests"])
+      requires=["numpy", "pygame","console_progressbar","requests","Pillow"])
