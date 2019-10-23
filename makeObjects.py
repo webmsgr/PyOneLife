c2cb355a046d96381b32e0f8e827dcd92cccfd94
Wackery <32279125+webmsgr@users.noreply.github.com>
2019-10-23 11:07:41 -0700

    Commit that will be useless

--- makeObjects.py
+++ makeObjects.py
@@ -1,3 +1,3 @@
-# this python script will take all the transitions,objects,sprites,animations etc into a single npz file
+# this python script will take all the transitions, objects, sprites, animations etc into a single npz file
 import numpy
 import pygame
