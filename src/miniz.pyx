95615d9c3014ea330dd0e9bed618b7a14cb8f906
Wackery <32279125+webmsgr@users.noreply.github.com>
2019-10-23 09:38:53 -0700

    Update miniz.pyx

--- src/miniz.pyx
+++ src/miniz.pyx
@@ -1,1 +1,3 @@
-
+cimport minizwrapper as miniz
+cpdef mz_uncompress(unsigned char *pDest, miniz.mz_ulong *pDest_len, const unsigned char *pSource, miniz.mz_ulong source_len):
+    miniz.mz_uncompress(pDest,pDest_len,pSource,source_len)
