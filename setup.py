9a8154081296c024361e65eed5ea5aea6805d6a1
Wackery <32279125+webmsgr@users.noreply.github.com>
2019-10-24 09:52:46 -0700

    Merge mapChunkparser into the main source (#9)
    * Update mapchunkparser.pyx
    
    * Update pyOHOL.pyx (#8)
    
    * Update setup.py
    
    * Delete mapchunkparser.pyx
    
    * Delete mapchunkparser.pxd
    
    * Update pyOHOL.pyx
    
    * Update pythonpackage.yml
    
    * Restyle Merge (#10)
    
    * Restyled by autopep8
    
    * Restyled by black
    
    * Restyled by reorder-python-imports

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import os
 import sys
 from distutils.core import Extension
 from distutils.core import setup
+
 import numpy as np
 from Cython.Build import cythonize
 
@@ -9,22 +10,20 @@ if sys.version[0] == 2:
     sys.stdout.write("No python 2 support\n")
     sys.exit(1)
 
+
 def dfunc(arg):
     return arg
 
 
 exts = cythonize(
-    [
-        Extension(
-            "pyOHOL",
-            ["src/pyOHOL.pyx", "src/mapchunkparser.pyx"],
-        )
-    ],
+    [Extension("pyOHOL", ["src/pyOHOL.pyx"])],
     compiler_directives={"language_level": "3"},
 )
 
-setup(name="pyOHOL",
-      version="1.0",
-      ext_modules=exts,
-      include_dirs = [np.get_include(),'./src/'],
-      requires=["numpy", "pygame","console_progressbar","requests","Pillow"])
+setup(
+    name="pyOHOL",
+    version="1.0",
+    ext_modules=exts,
+    include_dirs=[np.get_include(), "./src/"],
+    requires=["numpy", "pygame", "console_progressbar", "requests", "Pillow"],
+)
--- src/pyOHOL.pyx
+++ src/pyOHOL.pyx
@@ -7,7 +7,29 @@ import os
 import glob
 from PIL import Image
 DEF tilesize = 128
+cdef extern from "miniz.h":
+    pass
+cdef extern from "miniz.c":
+    int mz_uncompress(unsigned char *pDest, unsigned long *pDest_len, const unsigned char *pSource, unsigned long source_len);
+    
 
+cpdef parse_chunk(bytes header,bytes compressed):
+    cdef unsigned char *mpdata
+    cdef unsigned long *csize 
+    cdef unsigned long cbsize
+    cdef unsigned long before
+    cdef bytes out
+    cdef char beforetwo = compressed
+    cdef char *step2 = &(beforetwo)
+    cdef unsigned char *pSource = <unsigned char *>step2
+    header = header.split()
+    _, width, height, x, y, size, cbuffersize = header
+    cbsize = <unsigned long>int(cbuffersize)
+    before = <unsigned long>int(size)
+    csize = &(before)
+    mz_uncompress(mpdata,csize,pSource,cbsize)
+    out = <bytes>mpdata
+ 
 tilesperscreen = 5 # zoom
 
 from console_progressbar import ProgressBar
