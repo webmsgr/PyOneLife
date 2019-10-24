e6d26d9bb1a2860c4258c4fb82d1ca94010a7d33
Wackery Smackery <zapcontact000@gmail.com>
2019-10-23 17:37:58 -0700

    Add some macros

--- entrypoint.py
+++ entrypoint.py
@@ -1,8 +1,11 @@
 # use after installing the client to run the client
 import sys
+import multiprocessing
 try:
     import pyOHOL
 except ImportError as e:
     print("Client is not installed")
     raise e
-pyOHOL.main()
+if __name__ == "__main__":
+    multiprocessing.freeze_support()
+    pyOHOL.main()
--- src/pyOHOL.pyx
+++ src/pyOHOL.pyx
@@ -12,94 +12,120 @@ tilesperscreen = 5 # zoom
 from console_progressbar import ProgressBar
 
 def loadgrounds():
-    grounds = []
-    for file in glob.glob("./OneLifeData/ground/ground_*.tga"):
-        grounds.append(pygame.transform.scale(pygame.image.load(file),(tilesize,tilesize)))
-    return grounds
+	grounds = []
+	for file in glob.glob("./OneLifeData/ground/ground_*.tga"):
+		grounds.append(pygame.transform.scale(pygame.image.load(file),(tilesize,tilesize)))
+	return grounds
 
-cpdef display_process(pipe,grounds):
-    # Define some colors
-    BLACK = (0, 0, 0)
-    WHITE = (255, 255, 255)
-    GREEN = (0, 255, 0)
-    RED = (255, 0, 0)
-    pygame.init()
-    # Set the width and height of the screen [width, height]
-    size = (tilesize*tilesperscreen, tilesize*tilesperscreen)
-    screen = pygame.display.set_mode(size)
-    pygame.display.set_caption("PyOHOL")
-    # Loop until the user clicks the close button.
-    done = False
-    # Used to manage how fast the screen updates
-    clock = pygame.time.Clock()
-    screenSurface = pygame.Surface(size)
-    while not done:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
-        # command getter goes here
-        changed = False
-        while pipe.poll():
-            changed = True
-            command = pipe.recv()
-            if command == "STOP":
-                done = True
-            if command.startswith("DRAWPIXEL"):
-                command = command.split()
-                _ = command.pop(0)
-                x,y,r,g,b = map(int,command)
-                pygame.draw.line(screenSurface,(r,g,b),(x,y),(x,y))
-            if command.startswith("DRAWLINE"):
-                command = command.split()
-                _ = command.pop(0)
-                x1,y1,x2,y2,r,g,b = map(int,command)
-                pygame.draw.line(screenSurface,(r,g,b),(x1,y1),(x2,y2))
-            if command.startswith("DRAWFLOOR"):
-                command = command.split()
-                _ = command.pop(0)
-                x,y,id = map(int,command)
-                x,y = x*tilesize,y*tilesize
-                screenSurface.blit(grounds[id],(x,y))
-            pipe.send("OK")
-            # do things
-        if changed:
-            screen.fill(WHITE)
-            screen.blit(screenSurface,(0,0))
-            pygame.display.flip()
-        clock.tick(60)
-    pygame.quit()
+cpdef display_process(pipe):
+	grounds = loadgrounds()
+	pipe.send("READY")
+	# Define some colors
+	BLACK = (0, 0, 0)
+	WHITE = (255, 255, 255)
+	GREEN = (0, 255, 0)
+	RED = (255, 0, 0)
+	pygame.init()
+	# Set the width and height of the screen [width, height]
+	size = (tilesize*tilesperscreen, tilesize*tilesperscreen)
+	screen = pygame.display.set_mode(size)
+	pygame.display.set_caption("PyOHOL")
+	# Loop until the user clicks the close button.
+	done = False
+	# Used to manage how fast the screen updates
+	clock = pygame.time.Clock()
+	screenSurface = pygame.Surface(size)
+	while not done:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				done = True
+		# command getter goes here
+		changed = False
+		while pipe.poll():
+			changed = True
+			command = pipe.recv()
+			if command == "STOP":
+				done = True
+			if command.startswith("DRAWPIXEL"):
+				command = command.split()
+				_ = command.pop(0)
+				x,y,r,g,b = map(int,command)
+				pygame.draw.line(screenSurface,(r,g,b),(x,y),(x,y))
+			if command.startswith("DRAWLINE"):
+				command = command.split()
+				_ = command.pop(0)
+				x1,y1,x2,y2,r,g,b = map(int,command)
+				pygame.draw.line(screenSurface,(r,g,b),(x1,y1),(x2,y2))
+			if command.startswith("DRAWFLOOR"):
+				command = command.split()
+				_ = command.pop(0)
+				x,y,id = map(int,command)
+				x,y = x*tilesize,y*tilesize
+				screenSurface.blit(grounds[id],(x,y))
+			pipe.send("OK")
+			# do things
+		if changed:
+			screen.fill(WHITE)
+			screen.blit(screenSurface,(0,0))
+			pygame.display.flip()
+		clock.tick(60)
+	pygame.quit()
 
-
+	
+def groundtest():
+	return "DRAWFLOOR 0 0 0#DRAWFLOOR 1 0 1#DRAWFLOOR 2 0 2#DRAWFLOOR 3 0 3#DRAWFLOOR 4 0 4#DRAWFLOOR 0 1 5#DRAWFLOOR 1 1 6#DRAWFLOOR 2 1 7"	
+def fill(floor="0"):
+	out = []
+	for x in range(tilesperscreen):
+		for y in range(tilesperscreen):
+			out.append("DRAWFLOOR {} {} {}".format(x,y,floor))
+	return "#".join(out)
+def tile():
+	out = []
+	for x in range(tilesperscreen):
+		for y in range(tilesperscreen):
+			out.append("DRAWFLOOR {} {} {}".format(x,y,(y*(tilesperscreen-1)+x)%7))
+	return "#".join(out)
 macros = {
-    "GROUNDTEST":"DRAWFLOOR 0 0 0#DRAWFLOOR 1 0 1#DRAWFLOOR 2 0 2#DRAWFLOOR 3 0 3#DRAWFLOOR 4 0 4#DRAWFLOOR 0 1 5#DRAWFLOOR 1 1 6#DRAWFLOOR 2 1 7"
+	"GROUNDTEST": groundtest,
+	"FILLGRASS": lambda: fill("0"),
+	"FILLCLAY": lambda: fill("1"),
+	"FILLDESERT": lambda: fill("2"),
+	"FILLGRAVEL": lambda: fill("3"),
+	"FILLWHITE": lambda: fill("4"),
+	"FILLCRACKS": lambda: fill("5"),
+	"FILLJUNGLE": lambda: fill("6"),
+	"FILLUNKOWN": lambda: fill("7"),
+	"TILE": tile
 }
 
 
 def server_process(saddr,sport,pipe):
-    pass
+	pass
 def main():
-    display,d = mp.Pipe()
-    grounds = loadgrounds()
-    display_proc = mp.Process(target=display_process,args=(d,grounds))
-    display_proc.start()
-    m = ""
-    while display_proc.is_alive() and m != "q":
-        m = input(">")
-        if m.startswith("MACRO"):
-            m = m.split()
-            macroname = m[1]
-            if macroname in macros:
-                macroz = macros[macroname].split("#")
-                for macro in macroz:
-                    display.send(macro)
-                    display.recv()
-            continue
-        if m != "q":
-            display.send(m)
-            print(display.recv())
-    print("Stopping")
-    display.send("STOP")
-    display_proc.join()
+	display,d = mp.Pipe()
+	display_proc = mp.Process(target=display_process,args=(d,))
+	display_proc.start()
+	while not display.poll():
+		pass
+	m = ""
+	while display_proc.is_alive() and m != "q":
+		m = input(">")
+		if m.startswith("MACRO"):
+			m = m.split()
+			macroname = m[1]
+			if macroname in macros:
+				macroz = macros[macroname]().split("#")
+				for macro in macroz:
+					display.send(macro)
+					display.recv()
+			continue
+		if m != "q":
+			display.send(m)
+			print(display.recv())
+	print("Stopping")
+	display.send("STOP")
+	display_proc.join()
 
 if __name__ == "__main__":
-    main()
+	main()
